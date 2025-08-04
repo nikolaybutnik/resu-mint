f63a32ef3832b82c6872fc0ba69a0984ef058f08
Nikolay Butnik <btnk.nik@gmail.com>
2025-08-04 19:22:36 -0400

    Temporary defaults for supabase env keys

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/lib/types/database'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+// TODO: temporary measure to pass Vercel build. Update with prod keys
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://placeholder.supabase.co'
+const supabasePublishableKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || 'placeholder-key'
 
 export const supabase = createClient<Database>(
   supabaseUrl,
